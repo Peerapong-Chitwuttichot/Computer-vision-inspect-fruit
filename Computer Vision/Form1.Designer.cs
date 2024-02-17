0305e4afd3b4a29ca88a91cdda04e02e6f57202c
NaruebeTh1 <b6419615@g.sut.ac.th>
2024-02-17 23:29:44 +0700

    Add Project

---
+++ Computer Vision/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Computer_Vision
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button2.Location = new System.Drawing.Point(1081, 100);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(335, 85);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Start image processing";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Location = new System.Drawing.Point(25, 100);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(496, 340);
+            this.pictureBox4.TabIndex = 1;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox5.Location = new System.Drawing.Point(562, 505);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(496, 354);
+            this.pictureBox5.TabIndex = 2;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox6.Location = new System.Drawing.Point(562, 100);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(496, 340);
+            this.pictureBox6.TabIndex = 3;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.SystemColors.Info;
+            this.pictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox7.Location = new System.Drawing.Point(25, 505);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(496, 354);
+            this.pictureBox7.TabIndex = 4;
+            this.pictureBox7.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(173, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Fruit Picture";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label2.Location = new System.Drawing.Point(679, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 31);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Picture of good fruit";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label3.Location = new System.Drawing.Point(136, 455);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(234, 31);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Picture of bad fruit";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label4.Location = new System.Drawing.Point(679, 455);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(234, 31);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Picture of bad fruit";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1441, 887);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Computer Vision/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using Emgu.CV.CvEnum;
+
+namespace Computer_Vision
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+  
+        private Image<Bgr, byte> sourceImage;
+        private Image<Bgr, byte> sourceImage2;
+
+        private void inspectionFruit(string imagePath)
+        {
+            // อ่านรูปผลไม้
+            sourceImage = new Image<Bgr, byte>(imagePath);
+            sourceImage2 = new Image<Bgr, byte>(imagePath);
+
+            // ทำการประมวลผลเพื่อแยกส่วนที่ดีและเน่าของผลไม้
+            Mat src = sourceImage.Mat;
+            Mat thr = new Mat();
+            CvInvoke.CvtColor(src, thr, ColorConversion.Bgr2Gray);
+            CvInvoke.Threshold(thr, thr, 0, 255, ThresholdType.Otsu);
+
+            // Convert BGR to HSV
+            Mat HSV = new Mat();
+            CvInvoke.CvtColor(src, HSV, ColorConversion.Bgr2Hsv);
+
+            // Thresholding in HSV color space
+            Mat hsv_thr = new Mat();
+            CvInvoke.InRange(HSV, new ScalarArray(new MCvScalar(20, 10, 10)), 
+                new ScalarArray(new MCvScalar(90, 255, 255)), hsv_thr);
+
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            Mat hierarchy = new Mat();
+            int largest_contour_index = 0;
+            double largest_area = 0;
+            Mat mask = new Mat(src.Rows, src.Cols, DepthType.Cv8U, 1);
+            CvInvoke.FindContours(thr.Clone(), contours, hierarchy, 
+                RetrType.External, ChainApproxMethod.ChainApproxSimple);
+
+            for (int i = 0; i < contours.Size; i++)
+            {
+                double a = CvInvoke.ContourArea(contours[i], false);
+                if (a > largest_area)
+                {
+                    largest_area = a;
+                    largest_contour_index = i;
+                }
+            }
+
+            CvInvoke.DrawContours(mask, contours, largest_contour_index, new MCvScalar(255, 255, 255), -1, LineType.EightConnected, hierarchy);
+            int dilation_size = 2;
+            Mat element = CvInvoke.GetStructuringElement(ElementShape.Rectangle, new Size(2 * dilation_size + 1, 2 * dilation_size + 1), new Point(dilation_size, dilation_size));
+            CvInvoke.Erode(mask, mask, element, new Point(-1, -1), 1, BorderType.Default, new MCvScalar(0));
+
+            // Apply bitwise_not
+            Mat dst = new Mat();
+            CvInvoke.BitwiseNot(hsv_thr, dst, mask);
+
+            // Find contours in the inverted image
+            VectorOfVectorOfPoint contours_dst = new VectorOfVectorOfPoint();
+            Mat hierarchy_dst = new Mat();
+            CvInvoke.FindContours(dst.Clone(), contours_dst, hierarchy_dst, 
+                RetrType.External, ChainApproxMethod.ChainApproxSimple);
+
+            // Draw contours on the original image
+            for (int i = 0; i < contours_dst.Size; i++)
+            {
+                CvInvoke.DrawContours(src, contours_dst, i, 
+                    new MCvScalar(0, 0, 255), 1, 
+                    LineType.EightConnected, hierarchy_dst);
+            }
+
+            // ปรับขนาดรูปให้พอดีกับ PictureBox
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            // แสดงรูปภาพผลลัพธ์
+            pictureBox4.Image = sourceImage2.ToBitmap(); //แสดงรูปแอปเปิ้ล
+            pictureBox5.Image = dst.ToImage<Bgr, byte>().ToBitmap(); // แสดงส่วนที่ดีของแอปเปิ้ล
+            pictureBox6.Image = hsv_thr.ToImage<Bgr, byte>().ToBitmap(); // แสดงส่วนที่เน่าของแอปเปิ้ล
+            pictureBox7.Image = sourceImage.ToBitmap(); // แสดงส่วนที่เน่าของแอปเปิ้ล RGB
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Title = "Select Image File";
+            openFileDialog1.Filter = "Image Files (*.jpg; *.jpeg; *.png; *.gif)|*.jpg; *.jpeg; *.png; *.gif";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string ImagePath = openFileDialog1.FileName;
+                inspectionFruit(ImagePath);
+            }
+        }
+
+    }
+}
